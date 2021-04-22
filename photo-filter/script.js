9ae8c2269c8e7a5fec721f622932c63c835f9ee8
BertFrontEnd <Berton@yandex.ru>
2021-04-22 22:47:30 +0300

    feat: add change of fullscreen

---
+++ photo-filter/script.js
@@ -0,0 +1,26 @@
+import {
+  activateFullscreen,
+  deactivateFullscreen,
+} from './assets/module/fullscreen.js';
+
+const toFullscreenButton = document.querySelector('.open-fullscreen');
+const fromFullscreenButton = document.querySelector('.close-fullscreen');
+
+toFullscreenButton.addEventListener('click', () => {
+  activateFullscreen();
+  fromFullscreenButton.style.display = 'block';
+  toFullscreenButton.style.display = 'none';
+});
+
+fromFullscreenButton.addEventListener('click', () => {
+  deactivateFullscreen();
+  toFullscreenButton.style.display = 'block';
+  fromFullscreenButton.style.display = 'none';
+});
+
+window.addEventListener('fullscreenchange', () => {
+  if (!document.fullscreenElement) {
+    toFullscreenButton.style.display = 'block';
+    fromFullscreenButton.style.display = 'none';
+  }
+});
